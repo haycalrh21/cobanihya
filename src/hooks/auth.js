bfe856071dba264d4f9b6cae12f008eb0fc5f4dc
Your Name <hrayhansyah@gmail.com>
2024-02-24 21:42:04 +0700

    test

--- src/hooks/auth.js
+++ src/hooks/auth.js
@@ -18,7 +18,7 @@ export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
             }),
     )
 
-    const csrf = () => axios.get('/sanctum/csrf-cookie')
+    const csrf = () => axios.get('http://127.0.0.1:8000/sanctum/csrf-cookie')
 
     const register = async ({ setErrors, ...props }) => {
         await csrf()
