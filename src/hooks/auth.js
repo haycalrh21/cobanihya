2f19d6b579b9b1404329bcfda2dc44ed92468270
Your Name <hrayhansyah@gmail.com>
2024-02-24 21:39:37 +0700

    test

--- src/hooks/auth.js
+++ src/hooks/auth.js
@@ -7,7 +7,7 @@ export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
     const router = useRouter()
     const params = useParams()
 
-    const { data: user, error, mutate } = useSWR('/api/user', () =>
+    const { data: user, error, mutate } = useSWR('http://127.0.0.1:8000/api/user', () =>
         axios
             .get('/api/user')
             .then(res => res.data)
